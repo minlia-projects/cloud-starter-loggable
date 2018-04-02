d11639bf7c2228c60f00783e342902f361627ef4
Henry Lin <badcop@163.com>
2018-04-02 17:22:44 +0800

    up

---
+++ src/main/java/com/minlia/cloud/loggable/LoggerFormats.java
@@ -0,0 +1,38 @@
+package com.minlia.cloud.loggable;
+
+import lombok.*;
+
+/**
+ * <p>
+ * Logger message formats that can be replaced with the defaults.
+ * </p>
+ * There are list of placeholder's that can be used:
+ * <ul>
+ * <li>${method.name} - method name</li>
+ * <li>${method.args} - method arguments</li>
+ * <li>${method.result} - method results</li>
+ * <li>${method.duration} - method runtime duration</li>
+ * <li>${method.warn.duration} - method runtime warning duration to be displayed</li>
+ * <li>${error.class.name} - exception class name</li>
+ * <li>${error.message} - exception message</li>
+ * <li>${error.source.class.name} - exception source class name</li>
+ * <li>${error.source.line} - exception source line number that cause the exception</li>
+ * </ul>
+ *
+ * @author will
+ *
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoggerFormats {
+
+    private String enter;
+    private String warnBefore;
+    private String warnAfter;
+    private String after;
+    private String error;
+
+}
